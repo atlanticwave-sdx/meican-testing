dabe8cfaa0a3e7638017d2b78018e660442a5ff5
ampath <ampath@amlights-MacBook-Pro.local>
2024-04-30 21:06:34 +0530

    Group SDX Ciruit testing with read, update, and delete permissions

--- tests/test_sdx_circuit_group_permissions.py
+++ tests/test_sdx_circuit_group_permissions.py
@@ -37,6 +37,9 @@ class Test(ssl_handle.SSLBaseTest):
         sleep(1)
 
         self.driver.find_element(By.XPATH, "//tbody/tr[7]/td[3]/div[1]/ins[1]").click()
+        self.driver.find_element(By.XPATH, "//tbody/tr[7]/td[4]/div[1]/ins[1]").click()
+        self.driver.find_element(By.XPATH, "//tbody/tr[7]/td[5]/div[1]/ins[1]").click()
+
         self.driver.find_element(By.XPATH, "//button[contains(text(),'Save')]").click()
         sleep(1)
         check_element2 = WebDriverWait(self.driver, 10).until(
@@ -49,65 +52,16 @@ class Test(ssl_handle.SSLBaseTest):
         sleep(1)
         self.driver.find_element(By.XPATH, "//a/span[text()='SDX Circuits']").click()
         view_link = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//body/div[1]/aside[1]/section[1]/ul[1]/li[3]/ul[1]/li[2]/a[1]"))
+            EC.all_of(
+                EC.visibility_of_element_located(
+                    (By.XPATH, "//body/div[1]/aside[1]/section[1]/ul[1]/li[3]/ul[1]/li[2]/a[1]")),
+                EC.visibility_of_element_located(
+                    (By.XPATH, "//span[contains(text(),'Update')]")),
+                EC.visibility_of_element_located(
+                    (By.XPATH, "//span[contains(text(),'Delete')]"))
+            )
         )
-        assert view_link.is_displayed(), "View button is not displayed under SDX Circuits"
-
-
-        # Update check
-        self.driver.find_element(By.XPATH, "//li/a/span[text()='Users']").click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//span[contains(text(),'Groups')]").click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//tbody/tr[10]/td[2]/a[1]/span[1]").click()
-        sleep(1)
-
-        self.driver.find_element(By.XPATH, "//tbody/tr[7]/td[4]/div[1]/ins[1]").click()
-        self.driver.find_element(By.XPATH, "//button[contains(text(),'Save')]").click()
-        sleep(1)
-        check_element3 = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Group updated successfully')]"))
-        )
-        assert check_element3.is_displayed(), "Group updated successfully message not displayed"
-
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//a/span[text()='SDX Circuits']").click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//a/span[text()='SDX Circuits']").click()
-
-        update_link = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(
-                (By.XPATH, "//span[contains(text(),'Update')]"))
-        )
-        assert update_link.is_displayed(), "Update button is not displayed under SDX Circuits"
-
-
-        # Delete check
-        self.driver.find_element(By.XPATH, "//li/a/span[text()='Users']").click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//span[contains(text(),'Groups')]").click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//tbody/tr[10]/td[2]/a[1]/span[1]").click()
-        sleep(1)
-
-        self.driver.find_element(By.XPATH, "//tbody/tr[7]/td[5]/div[1]/ins[1]").click()
-        self.driver.find_element(By.XPATH, "//button[contains(text(),'Save')]").click()
-        sleep(1)
-        check_element4 = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Group updated successfully')]"))
-        )
-        assert check_element4.is_displayed(), "Group updated successfully message not displayed"
-
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//a/span[text()='SDX Circuits']").click()
-        sleep(1)
-        self.driver.find_element(By.XPATH, "//a/span[text()='SDX Circuits']").click()
-
-        delete_link = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(
-                (By.XPATH, "//span[contains(text(),'Delete')]"))
-        )
-        assert delete_link.is_displayed(), "Delete button is not displayed under SDX Circuits"
+        assert view_link is not None, "View/Update/Delete buttons not displayed under SDX Circuits"
 
 if __name__ == "__main__":
     pytest.main()
