1a7af361f60f611845f3de7e36dd75fe088113c9
mahidhar <mgand024@fiu.edu>
2024-10-31 10:45:15 -0400

    Updating the Orcid signup test case with URLs

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,9 @@
 all_urls = {
-    'ADMIN_LOGIN_URL': 'http://localhost/aaa/login/admin',
-    'USER_LOGIN_URL': 'http://localhost',
-    'API_URL': 'http://67.17.206.206:8080/SDX-Controller/1.0.0/'
+    'ADMIN_LOGIN_URL': 'http://127.0.0.1/aaa/login/admin',
+    'USER_LOGIN_URL': 'http://127.0.0.1',
+    'API_URL': 'http://67.17.206.205:8080/SDX-Controller/1.0.0/',
+    'ORCID_VALID_USERNAME' : 'XXXX',
+    'ORCID_VALID_USERNAME_PASSWORD' : 'XXXX',
+    'ORCID_USER_EMAIL' : 'XXXX'
 }
 
